138a42f8bad77eef017230b7af89e696f4cd62bf
rahulk <rahul.k@codecraft.co.in>
2024-03-04 22:21:11 +0530

    img crop

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import offerImage from '../../assets/images/offer-image.jpg'
-import './offer.scss'
+import offerImage from "../../assets/images/offer-image.jpeg";
+import "./offer.scss";
 import Button from "../Button";
 
 function Offer() {
@@ -11,7 +11,8 @@ function Offer() {
           <h1 className="title">Offer-Promotion</h1>
           <h3>First-Time Head Wash Special</h3>
           <p>
-          "Debut Delight: Elevate Your Scalp Game with Our First-Time Head Wash Promotion!"
+            "Debut Delight: Elevate Your Scalp Game with Our First-Time Head
+            Wash Promotion!"
           </p>
           <div className="more-info">
             <h2>45 &#8377;</h2>
@@ -21,7 +22,7 @@ function Offer() {
           </div>
         </div>
         <div className="image-container">
-        <div className="offer-overlay"></div>
+          <div className="offer-overlay"></div>
           <img src={offerImage} alt="offer-image" />
         </div>
       </div>
