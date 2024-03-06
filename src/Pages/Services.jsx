43cb726aec81d385c584034f4d9d7e283461ff6f
rahulk <rahul.k@codecraft.co.in>
2024-03-06 23:53:04 +0530

    new bug fixes

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -5,11 +5,15 @@ import Footer from "../components/Footer/Footer";
 import ServiceContainer from "../components/ServiceContainer/index";
 import ScrollToTop from "../components/ScrollToTop/index";
 import ComingSoon from "../components/ComingSoon";
+import Whatsapp from "../assets/svg/Whatsapp";
 
 const Services = () => {
   return (
     <>
       <ComingSoon />
+      <a target="_blank" rel="noreferrer" href="https://wa.me/+917829780587">
+        <Whatsapp />
+      </a>
       <ScrollToTop />
       <Hero />
       <ServiceContainer />
