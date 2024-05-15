766873a5409fd822a5a2cdf62318d6fbadbf2c83
rahulk216 <rahulkakkadan216@gmail.com>
2024-05-15 14:39:09 +0530

    owner message

--- src/components/MessageOwner/index.jsx
+++ src/components/MessageOwner/index.jsx
@@ -10,18 +10,46 @@ const MessageOwner = () => {
         <div className="owner-text">
           <h1>A message from our owner</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis porro
-            delectus quasi nam ducimus quod sequi incidunt impedit veniam nihil?
-            Eligendi at accusantium deleniti iusto veritatis minima fugit.
-            Saepe, modi autem quo quisquam reprehenderit, eius explicabo vitae,
-            earum dolorem placeat est. Enim, quam illo? Ipsam dolore libero
-            delectus aliquam nostrum quaerat reiciendis expedita at odio,
-            dolores blanditiis harum dolorem atque dignissimos fuga facilis a
-            unde, maxime voluptatum? Perspiciatis deleniti voluptates cum
-            excepturi quo consequuntur magni quibusdam impedit earum non! Quia
-            sapiente expedita dolore quam voluptatum recusandae ullam deserunt
-            est, consequatur corrupti impedit optio, non ducimus! Eveniet
-            tempora accusamus soluta nemo.
+            <p>Dear Valued Customers,</p>
+            <p>
+              I am thrilled to welcome you to our salon, where we strive to
+              exceed your expectations with every visit. Our team is dedicated
+              to providing you with the highest level of service and ensuring
+              that you leave feeling pampered and rejuvenated.
+            </p>
+            <p>
+              At Zarah Elite, we understand the importance of self-care and the
+              role it plays in enhancing your overall well-being. Our commitment
+              to excellence drives us to continually innovate and bring you the
+              latest trends and techniques in the beauty industry.
+            </p>
+            <p>
+              As we navigate these challenging times, rest assured that your
+              safety and comfort are our top priorities. We have implemented
+              stringent hygiene protocols and safety measures to create a serene
+              and clean environment for your relaxation.
+            </p>
+            <p>
+              I would like to express my gratitude for your continued support
+              and trust in us. Your satisfaction is our motivation, and we look
+              forward to serving you and helping you feel your best.
+            </p>
+            <p>
+              Thank you for choosing Zarah Elite. Sit back, relax, and let us
+              pamper you!
+            </p>
+            <p>Warm regards,</p>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>
+                <strong>Mohammed Shanakath Ali</strong>
+              </span>
+              <span>
+                <strong>Managing Director</strong>
+              </span>
+              <span>
+                <strong>Zarah Elite Professional Unisex Salon </strong>
+              </span>
+            </div>
           </p>
         </div>
         <div className="owner-img">
